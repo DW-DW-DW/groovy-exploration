fb15a841070b457026e93f1a0fc17dda8855eae7
DW-DW-DW <hillbergsara@gmail.com>
2017-04-28 23:22:25 +0100

    some basics

--- main.groovy
+++ main.groovy
@@ -1,1 +1,36 @@
 println "testing Groovy"
+
+def dogs = ["Spot", "Lassie", "Laika"]
+
+for (dog in dogs) {
+	println "$dog"
+}
+
+def rankings = 1..10
+println rankings
+
+def enum DAYS {
+	MON,
+	TUE,
+	WED,
+	THU,
+	FRI,
+	SAT,
+	SUN
+}
+
+def weekdays = DAYS.MON..DAYS.FRI
+
+println weekdays.from
+println weekdays.to
+
+println greet("Daisy")
+
+def greet(name) {
+	"Hello " + name //don't even need the return statement
+}
+
+def testClosure = { println Math.random() }
+testClosure()
+testClosure()
+
